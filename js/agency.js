9d588c7ec042ded5ae57a0e7045b7722100b8ac8
Code_Caster <roma.pasko@gmail.com>
2017-07-15 19:10:28 +0300

    second

--- js/agency.js
+++ js/agency.js
@@ -43,7 +43,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(49.610766, 34.532474));
 });
 
 function init() {
@@ -54,7 +54,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(49.610766, 34.532474), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -182,7 +182,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(49.610766, 34.532474);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
